d0ccd36bad55d697b8c98205e6c4e9992eefd614
Prasun Ray <p.ray@imperial.ac.uk>
2019-02-04 19:58:32 +0000

    adding corrected bfs code

--- codes/bfs.py
+++ codes/bfs.py
@@ -1,12 +1,12 @@
-""" Simple bfs code from lecture 7
+""" Simple bfs code from lecture 7 (corrected version)
 """
 import networkx as nx
 
-def bfs(G,x):
+def bfs(G,s):
     """
     Input:
     G: networkx graph
-    x: source node
+    s: source node
 
     Output:
     L2: Labels for all nodes in graph, 0=unreachable from source, 1=reachable
@@ -15,14 +15,13 @@ def bfs(G,x):
 
     L1 = list(G.nodes) #Assumes nodes are numbered from 0 to N-1
     L2 = [0 for l in L1]
-    L2 = [0 for l in L1]
     L3 = [-1000 for l in L1]
 
     Q=[]
-    s = 1
     Q.append(s)
     L2[s]=1
     L3[s]=0
+
     while len(Q)>0:
         x = Q.pop(0)
         for v in G.adj[x].keys():
