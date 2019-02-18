cef24a13d4d87370bc90bb67993a7ce854a67d52
Prasun Ray <p.ray@imperial.ac.uk>
2019-02-18 20:25:35 +0000

    adding hw2 template files

---
+++ hw2/p1_template.py
@@ -0,0 +1,99 @@
+"""M345SC Homework 2, part 1
+Your name and CID here
+"""
+
+def scheduler(L):
+    """
+    Question 1.1
+    Schedule tasks using dependency list provided as input
+
+    Input:
+    L: Dependency list for tasks. L contains N sub-lists, and L[i] is a sub-list
+    containing integers (the sub-list my also be empty). An integer, j, in this
+    sub-list indicates that task j must be completed before task i can be started.
+
+    Output:
+    L: A list of integers corresponding to the schedule of tasks. L[i] indicates
+    the day on which task i should be carried out. Days are numbered starting
+    from 0.
+
+    Discussion: Add analysis here
+    """
+
+
+    S=[] #Modify as needed
+
+    return S
+
+
+def findPath(A,a0,amin,J1,J2):
+    """
+    Question 1.2 i)
+    Search for feasible path for successful propagation of signal
+    from node J1 to J2
+
+    Input:
+    A: Adjacency list for graph. A[i] is a sub-list containing two-element tuples (the
+    sub-list my also be empty) of the form (j,Lij). The integer, j, indicates that there is a link
+    between nodes i and j and Lij is the loss parameter for the link.
+
+    a0: Initial amplitude of signal at node J1
+
+    amin: If a>=amin when the signal reaches a junction, it is boosted to a0.
+    Otherwise, the signal is discarded and has not successfully
+    reached the junction.
+
+    J1: Signal starts at node J1 with amplitude, a0
+    J2: Function should determine if the signal can successfully reach node J2 from node J1
+
+    Output:
+    L: A list of integers corresponding to a feasible path from J1 to J2.
+
+    Discussion: Add analysis here
+    """
+
+
+    L=[] #Modify as needed
+
+    return L
+
+
+def a0min(A,amin,J1,J2):
+    """
+    Question 1.2 ii)
+    Find minimum initial amplitude needed for signal to be able to
+    successfully propagate from node J1 to J2 in network (defined by adjacency list, A)
+
+    Input:
+    A: Adjacency list for graph. A[i] is a sub-list containing two-element tuples (the
+    sub-list my also be empty) of the form (j,Lij). The integer, j, indicates that there is a link
+    between nodes i and j and Lij is the loss parameter for the link.
+
+    amin: Threshold for signal boost
+    If a>=amin when the signal reaches a junction, it is boosted to a0.
+    Otherwise, the signal is discarded and has not successfully
+    reached the junction.
+
+    J1: Signal starts at node J1 with amplitude, a0
+    J2: Function should determine min(a0) needed so the signal can successfully
+    reach node J2 from node J1
+
+    Output:
+    (a0min,L) a two element tuple containing:
+    a0min: minimum initial amplitude needed for signal to successfully reach J2 from J1
+    L: A list of integers corresponding to a feasible path from J1 to J2 with
+    a0=a0min
+    If no feasible path exists for any a0, return output as shown below.
+
+    Discussion: Add analysis here
+    """
+
+
+    output = -1,[] #Modify as needed
+
+    return output
+
+
+if __name__=='__main__':
+    #add code here if/as desired
+    L=None #modify as needed
