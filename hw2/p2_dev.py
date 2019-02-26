e393d3f3fad36bc7089e5ce5051a855d86fb6b89
Anas <al2916@ic.ac.uk>
2019-02-26 00:42:10 +0000

    part2.2

--- hw2/p2_dev.py
+++ hw2/p2_dev.py
@@ -7,6 +7,7 @@ from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
 
+
 def model1(G,x=0,params=(50,80,105,71,1,0),tf=6,Nt=400,display=False):
     """
     Question 2.1
@@ -45,8 +46,8 @@ def infection(x,t,params=(50,80,105,71,1,0)):
 
     theta = theta1+theta2*(1-np.sin(2*np.pi*t))
 
-    didt = theta*s*v - (k + a)*i
     dvdt = k*(1 - v) - theta*(s*v)
+    didt = theta*s*v - (k + a)*i
     dsdt = a*i - (g + k)*s
 
     return [dvdt,didt,dsdt]
@@ -69,30 +70,84 @@ def modelN(G,x=0,params=(50,80,105,71,1,0.01),tf=6,Nt=400,display=False):
     Smean,Svar: Array containing mean and variance of S across network nodes at
                 each time step.
     """
-    a,theta1,theta2,k,g,tau=params
+    a,theta0,theta1,g,k,tau=params
     tarray = np.linspace(0,tf,Nt+1)
     Smean = np.zeros(Nt+1)
     Svar = np.zeros(Nt+1)
+    N = nx.number_of_nodes(G)           # Defining number of nodes
+    #x0_a = [0.1,0.05,0.05]
+    #x0_b = [1,0,0]*(N - 1)
+    #y = x0_a + x0_b
+    y_S = np.zeros(N)
+    y_I = np.zeros(N)
+    y_V = np.ones(N)
+    y_S[x] = 0.05
+    y_I[x] = 0.05
+    y_V[x] = 0.1
 
-    #Add code here
+    z = np.concatenate([y_S,y_I])
+    y = np.concatenate([z,y_V])
+
+
+
+    print(y.shape)
+    #y = [0.05] + [0] * 99 + [0.05] + [0] * 99 + [0.1] + [1] * 99
+
+    #[0.05,0.05,0.1] or [0,0,1]
+
+    #y = [0.05] + [0] * 99 + [0.05] + [0] * 99 + [0.1] + [1] * 99
+
+
+
+    #-------------------- Defining the adjacency matrix --------------------
+    A = nx.adjacency_matrix(G).toarray()
+    q_i = np.array(A.sum(axis=0))
+    sum_kj = np.array(np.dot(q_i,A))
+    flux_ij = np.array(tau*np.divide((np.multiply(q_i,A)),(sum_kj)))
+
+
+    #S = np.array(y[:N])
+    #I = np.array(y[N:2*N])
+    #V = np.array(y[2*N:3*N])
 
     def RHS(y,t):
         """Compute RHS of model at time t
         input: y should be a 3N x 1 array containing with
-        y[:N],y[N:2*N],y[2*N:3*N] corresponding to
+        S,I,V corresponding to
         S on nodes 0 to N-1, I on nodes 0 to N-1, and
         V on nodes 0 to N-1, respectively.
         output: dy: also a 3N x 1 array corresponding to dy/dt
 
         Discussion: add discussion here
         """
+        a,theta0,theta1,g,k,tau=params      # Input parameters
+        #N = nx.number_of_nodes(G)           # Defining number of nodes
+        S = np.array(y[:N])
+        I = np.array(y[N:2*N])
+        V = np.array(y[2*N:3*N])
+        dy = np.zeros(3*N)                  # Initializing dy
 
-        dy = 0 #modify
+        theta = theta0 + theta1*(1 - np.sin(2*np.pi*t))
+
+
+        dy[:N] = a*I - (g + k)*S + np.dot(np.array(flux_ij.sum(axis=0)),S) - np.multiply(np.array(flux_ij.sum(axis=0)),S)
+        dy[N:2*N] = theta*np.multiply(S,V) - (k + a)*I + np.dot(np.array(flux_ij.sum(axis=0)),I) - np.multiply(np.array(flux_ij.sum(axis=0)),I)
+        dy[2*N:3*N] = k - k*V - theta*np.multiply(S,V) + np.dot(np.array(flux_ij.sum(axis=0)),V) - np.multiply(np.array(flux_ij.sum(axis=0)),V)
+
+
         return dy
 
+    S_a = odeint(RHS,y,tarray)
+
+    S = S_a[:,:N]
+
+    Smean = np.mean(S,axis=1)
+
+    plt.scatter(tarray,Smean)
 
 
-    return Smean,Svar
+
+    return Smean
 
 
 def diffusion(input=(None)):
